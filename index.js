5784858a3dd00cbd056522a3dedde333f392234d
juo0o <https://github.com/juo0o>
2023-02-22 10:15:00 +0900

    nodejs practice first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+setTimeout(() => {
+    var figlet = require('figlet');
+
+figlet('node.js practice!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+}, 5000);
+
+
+
+const express = require('express')
+const app = express()
+
+
+app.get('/', function (req, res) {
+  res.send('Nodejs prac')
+})
+
+app.get('/dog', function (req, res) {
+    res.send('dog')
+})
+
+
+app.listen(3000)
